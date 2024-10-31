149dfbb8d1ede15dcb43663ba82ad3fffe7fa846
Heitor Previatti <previahc@mail.uc.edu>
2024-10-31 08:12:19 -0400

    Heitor

--- HalloweenCandy/mainPackage/main.py
+++ HalloweenCandy/mainPackage/main.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
     myFavorite = HalloweenCandy("Bill", "Chocolate")
     print(myFavorite)   # Invokes __str__ by default
     
+    yFavorite = HalloweenCandy("Heitor", "Bis")
+    print(myFavorite)   # Invokes __str__ by default
+    
